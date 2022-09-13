5929b4789a0ca64076012a4ba6ce4b09e961d86c
Durgaparasadkalla <Durga.kalla@northfacein.com>
2022-09-13 16:34:57 +0530

    latestversion

---
+++ leave-management/src/main/java/com/nfcs/leavemanagement/dto/UserDto.java
@@ -0,0 +1,19 @@
+package com.nfcs.leavemanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDto {
+
+	private Long user_id;
+	private String username;
+	private String dataOfBirth;
+	private String email;
+	private String address;
+	private String phoneNumber;
+	private String password;
+}
