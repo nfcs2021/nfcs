4f37a1b6c30295689470db980a15ae5f12e2495a
Durgaparasadkalla <Durga.kalla@northfacein.com>
2022-09-13 17:32:21 +0530

    spring-boot

---
+++ nfcshcm/src/main/java/com/nfcs/hcm/NfcshcmApplication.java
@@ -0,0 +1,13 @@
+package com.nfcs.hcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfcshcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfcshcmApplication.class, args);
+	}
+
+}
