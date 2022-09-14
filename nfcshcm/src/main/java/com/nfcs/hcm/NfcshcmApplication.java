74e8b772d41e8be177ace2fe044a9be36b312d0f
Durgaparasadkalla <Durga.kalla@northfacein.com>
2022-09-14 10:34:14 +0530

    spring -boot

--- nfcshcm/src/main/java/com/nfcs/hcm/NfcshcmApplication.java
+++ nfcshcm/src/main/java/com/nfcs/hcm/NfcshcmApplication.java
@@ -1,7 +1,15 @@
 package com.nfcs.hcm;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class NfcshcmApplication {
@@ -10,4 +18,18 @@ public class NfcshcmApplication {
 		SpringApplication.run(NfcshcmApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean simpleCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		// *** URL below needs to match the Vue client URL and port ***
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
 }
