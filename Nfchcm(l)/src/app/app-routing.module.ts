ba35191df435cbf86cbd8e5a18cd90ae7d9d7df2
kvenkatakrishna <venkata.krishna@northfacein.com>
2022-09-14 10:01:41 +0530

    updated

--- Nfchcm(l)/src/app/app-routing.module.ts
+++ Nfchcm(l)/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 const routes: Routes = [
-  {path: 'login', component:LoginComponent},
+  {path: '', component:LoginComponent},
 ];
 
 @NgModule({
---
+++ Nfchcm(l)/src/app/components/password-generation/password-generation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-generation',
+  templateUrl: './password-generation.component.html',
+  styleUrls: ['./password-generation.component.css']
+})
+export class PasswordGenerationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
