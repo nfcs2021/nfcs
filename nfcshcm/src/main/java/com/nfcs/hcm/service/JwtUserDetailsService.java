9fb16d220fb737cdf68ba7e3b9aeb7baf6b2e227
Durgaparasadkalla <Durga.kalla@northfacein.com>
2022-09-20 15:21:55 +0530

    latest update

--- nfcshcm/src/main/java/com/nfcs/hcm/service/JwtUserDetailsService.java
+++ nfcshcm/src/main/java/com/nfcs/hcm/service/JwtUserDetailsService.java
@@ -11,16 +11,19 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.nfcs.hcm.dto.EmployeeDto;
 import com.nfcs.hcm.dto.UserDto;
+import com.nfcs.hcm.model.Employee;
 import com.nfcs.hcm.model.UserDao;
+import com.nfcs.hcm.repository.EmployeeRepository;
 import com.nfcs.hcm.repository.UserRepository;
-
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 	@Autowired
 	private UserRepository userDao;
-
+	@Autowired
+	private EmployeeRepository employeeRepository;
 	@Autowired
 	private PasswordEncoder bcryptEncoder;
 
@@ -42,6 +45,35 @@ public class JwtUserDetailsService implements UserDetailsService {
 		newUser.setPhoneNumber(user.getPhoneNumber());
 		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
 		return userDao.save(newUser);
+	}
+
+	public Employee save(EmployeeDto employeeDto) {
+		Employee employee = new Employee();
+		employee.setEmpNoSeries(employeeDto.getEmpNoSeries());
+		employee.setEmpname(employeeDto.getEmpname());
+		employee.setEmpNo(employeeDto.getEmpNo());
+		employee.setDateOfBirth(employee.getDateOfBirth());
+		employee.setGender(employeeDto.getGender());
+		employee.setReportingManager(employeeDto.getReportingManager());
+		employee.setStatus(employeeDto.getStatus());
+		employee.setDateOfJoining(employeeDto.getDateOfJoining());
+		employee.setProbationPeriod(employeeDto.getProbationPeriod());
+		employee.setConfirmationDate(employeeDto.getConfirmationDate());
+		employee.setEmail(employeeDto.getEmail());
+		employee.setPhoneNumber(employeeDto.getPhoneNumber());
+		employee.setEmergencyContactName(employeeDto.getEmergencyContactName());
+		employee.setEmergencyContactNumber(employeeDto.getEmergencyContactNumber());
+		employee.setFatherName(employeeDto.getFatherName());
+		employee.setSpouseName(employeeDto.getSpouseName());
+		employee.setDivision(employeeDto.getDivision());
+		employee.setCostCenter(employeeDto.getCostCenter());
+		employee.setGrade(employeeDto.getGrade());
+		employee.setDesignation(employeeDto.getDesignation());
+		employee.setLocation(employeeDto.getLocation());
+		employee.setDepartment(employeeDto.getDepartment());
+		employee = employeeRepository.save(employee);
+		employeeDto.setEmpId(employee.getEmpId());
+		return employee;
 	}
 
 }
