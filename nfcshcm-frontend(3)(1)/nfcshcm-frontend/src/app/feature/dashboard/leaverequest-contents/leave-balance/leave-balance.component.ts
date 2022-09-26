97976fcf103a024720eb2326849a18e5cdf289b5
venu <venu.nallamilli@northfacein.com>
2022-09-26 14:54:40 +0530

    updated

---
+++ nfcshcm-frontend(3)(1)/nfcshcm-frontend/src/app/feature/dashboard/leaverequest-contents/leave-balance/leave-balance.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmployeeService } from '../../services/employee.service';
+import { EmployeeLeaveService } from '../../services/employeeLeave.service';
+
+@Component({
+  selector: 'app-leave-balance',
+  templateUrl: './leave-balance.component.html',
+  styleUrls: ['./leave-balance.component.css']
+})
+export class LeaveBalanceComponent implements OnInit {
+  employeeLeaveData: any;
+ 
+  
+  constructor( private router: Router,
+    private employeeLeaveService:EmployeeLeaveService) { }
+
+  ngOnInit() {
+    this.router.navigate(['/home/leaverequests/leavebalance']);
+    
+    this.getEmployeeLeaveById(localStorage.getItem("loginEmployeeData"));
+    
+  }
+
+  getEmployeeLeaveById(empId) {
+    if (empId > 0) {
+      this.employeeLeaveService.getEmployeeLeaveById(empId)
+        .subscribe(
+          data => {
+this.employeeLeaveData=data;
+           console.log(data);
+           
+            // console.log('selectedEmployee data: ', data);
+          }, error => {
+           
+            console.log(error);
+            
+          });
+    } else {
+      
+    }
+  }
+
+}
