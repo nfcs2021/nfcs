3787f828a488d08bee8d578b5893e3bea53eb293
Durgaparasadkalla <Durga.kalla@northfacein.com>
2022-10-20 17:50:39 +0530

    latest version

--- personalizedPainTreatment/src/app/feature/dashboard/frontdesk-login/frontdesk-login.component.ts
+++ personalizedPainTreatment/src/app/feature/dashboard/frontdesk-login/frontdesk-login.component.ts
@@ -62,6 +62,7 @@ export class FrontdeskLoginComponent implements OnInit {
     this.authservice.loginUser(data).subscribe(
       (data) => {
         console.log('data' + data);
+        console.log('role',data.data.role)
         localStorage.setItem('token', data.access_token);
        console.log(data.data);
        localStorage.setItem('name',data.data.First_Name+data.data.Last_Name)
